5fad607127c3c6aeb1dcc806358b301414c3f48a
caufieldjh <j.harry.caufield@gmail.com>
2023-05-17 15:33:25 -0700

    Use langchain's LLMChain and template

--- src/kmeow/guide/describe.py
+++ src/kmeow/guide/describe.py
@@ -3,21 +3,22 @@ import os
 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 def describe_workflow(params: dict):
     """Describe workflow(s)."""
 
     api_key = os.environ.get("POETRY_OPENAI_API_KEY")
 
-    prompt = f"""
-    I am a computational biologist and I have a set of
-    sequencing results from an environmental isolate.
-    My results are in {params["format"]} format.
-    I would like to use the KBase tools to process and analyze
-    my sequences. What is the first tool I should use,
-    and what parameters should I use?
-    """
+    intro_template = PromptTemplate(input_variables=['format'],
+                                    template="""
+                                    I am a computational biologist and I have a set of
+                                    sequencing results from an environmental isolate.
+                                    My results are in {format} format.
+                                    I would like to use the KBase tools to process and analyze
+                                    my sequences. What is the first tool I should use,
+                                    and what parameters should I use?
+                                    """)
 
     llm = OpenAI(
         temperature=0.9,
@@ -25,6 +26,8 @@ def describe_workflow(params: dict):
         openai_api_key=api_key,
     )
 
-    response = llm(prompt)
+    workflow_chain = LLMChain(llm=llm, prompt=intro_template, verbose=True)
+
+    response = workflow_chain.run(format=params['format'])
 
     print(response)
