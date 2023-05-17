082ee75468835edd294e61fcb9a5073954cfdced
caufieldjh <j.harry.caufield@gmail.com>
2023-05-17 13:21:35 -0700

    Expand CLI and API key command

--- src/kmeow/cli.py
+++ src/kmeow/cli.py
@@ -15,15 +15,14 @@ def main(verbose: bool):
     :param verbose: bool, verbose output if used.
     """
     logger = logging.getLogger()
-    if verbose >= 2:
+    if verbose:
         logger.setLevel(level=logging.DEBUG)
-    elif verbose == 1:
-        logger.setLevel(level=logging.INFO)
     else:
         logger.setLevel(level=logging.WARNING)
     logger.info(f"Logger {logger.name} set to level {logger.level}")
 
-@click.argument("input", required=True)
+@main.command()
+@click.argument("api_key", required=True)
 def store_key(api_key: str):
     """Utility for storing OpenAI API key."""
     store_api_key(api_key)
